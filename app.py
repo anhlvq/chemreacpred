8474038c72ddcdf84c528d2bc8787d1b5028ac4c
anhlvq <anhlvq0@gmail.com>
2020-10-28 13:47:00 +0700

    dataset class

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import plotly.express as px
 import pandas as pd
 
 
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
