ea12e7e30a42538c2b13d0a488083bf1b445863d
anhlvq <anhlvq0@gmail.com>
2020-10-28 15:26:54 +0700

    dataset class

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import pandas as pd
 
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash()
 
 #df = pd.read_csv(
 #    'https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
